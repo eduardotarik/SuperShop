74c097efb6b1a1ef28622fa0e1456c0cee1b4f43
eduardotarik <eduardotarik@outlook.pt>
2023-05-18 19:57:03 +0100

    Add project files.

---
+++ SuperShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SuperShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
