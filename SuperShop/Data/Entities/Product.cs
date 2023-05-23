dd18f96618b86b2cf7306e4710ee0599eb351134
eduardotarik <eduardotarik@outlook.pt>
2023-05-23 16:34:22 +0100

    SeedDb

--- SuperShop/Data/Entities/Product.cs
+++ SuperShop/Data/Entities/Product.cs
@@ -1,11 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace SuperShop.Data.Entities
+﻿namespace SuperShop.Data.Entities
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class Product
     {
         public int Id { get; set; }
 
+        [Required]
+        [MaxLength(50, ErrorMessage = "The field {0} can contain {1} characters length.")]
         public string Name { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
@@ -15,10 +17,10 @@ namespace SuperShop.Data.Entities
         public string ImageUrl { get; set; }
 
         [Display(Name = "Last Purchase")]
-        public DateTime LastPurchase { get; set; }
+        public DateTime? LastPurchase { get; set; }
 
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
 
         [Display(Name = "Is Available")]
         public bool IsAvailable { get; set; }
--- SuperShop/Program.cs
+++ SuperShop/Program.cs
@@ -11,7 +11,13 @@ builder.Services.AddDbContext<DataContext>(cfg =>
     cfg.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddTransient<SeedDb>();
+
 var app = builder.Build();
+
+
+
+RunSeeding(app);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -33,3 +39,17 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+void RunSeeding(IApplicationBuilder app)
+{
+    using (var scope = app.ApplicationServices.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        var dbContext = services.GetRequiredService<DataContext>();
+
+        var seeder = new SeedDb(dbContext);
+        seeder.SeedAsync().Wait();
+    }
+}
+
+
